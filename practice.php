854dc3d1749061342342f94356cbb102f110851c
yoshbatula <yoshbatula2@gmail.com>
2025-01-31 00:44:15 +0800

    still progressing for php

--- practice.php
+++ practice.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP<?php</title>
+    <title>PHP</title>
 </head>
 <body>
-    
+    <form action="" method="POST">
+        <label for="age">Age Checker:</label><br>
+        <input type="text" name="age" required><br>
+        <input type="submit" value="check">
+    </form>
+
+    <?php
+        $age = $_POST["age"];
+
+        if ($age >= 18) {
+            echo "Your age is legal <br>";
+        } elseif($age < 18) {
+            echo "Your a minnor <br>";
+        } else {
+            echo "Please fill out the inputs <br>";
+        }
+    ?>
 </body>
 </html>
