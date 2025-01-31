9d93188e7057a47279fcf4da9f1e63daf9caefd7
yoshbatula <yoshbatula2@gmail.com>
2025-02-01 00:14:38 +0800

    progress for activities

--- login.php
+++ login.php
@@ -6,14 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    <form method="get">
-        <label for="email">Email</label>
-        <input type="text" name="email">
-        <label for="password">Password</label>
+    <form action="" method="POST">
+        <label for="email">Username: </label>
+        <input type="text" name="email"><br>
+        <label for="password">Password: </label>
         <input type="password" name="password">
         <input type="submit" value="submit">
     </form>
 
+    <?php 
+        $user = "admin";
+        $pass = "1234";
+
+        if (isset($_POST["email"])) {
+            echo"Please input password";
+        } header(url("process.php"));
+    ?>
     
 </body>
 </html>
