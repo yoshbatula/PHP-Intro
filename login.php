5332a8bf42aae9d032889ee6ae5f721704922485
yoshbatula <yoshbatula2@gmail.com>
2025-02-01 01:09:55 +0800

    yeyy finish na php

--- login.php
+++ login.php
@@ -19,8 +19,28 @@
         <br><br>
 
         <button type="submit">Login</button>
-        <?php include 'process.php'; ?>
 
+        <?php
+
+    $correctUsername = "admin";
+    $correctPassword = "1234";
+
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
+
+    
+    if ($username === $correctUsername && $password === $correctPassword) {
+        header("Location: login.php");
+        echo "<h2>Welcome, $username!</h2>";
+    } else {
+        echo "<h2>Error: Invalid username or password.</h2>";
+        echo "<a href='login.php'>Go back to Login</a>";
+    }
+} 
+?>
     </form>
 
 </body>
