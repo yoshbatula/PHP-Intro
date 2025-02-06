5ed3be303c5eb39a3d6e36509fd3551a382f9133
yoshbatula <yoshbatula2@gmail.com>
2025-02-07 00:03:07 +0800

    progress for todays vedyow

--- practice.php
+++ practice.php
@@ -1,4 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Odin Project</title>
+</head>
+<body>
+    <ul>
+        <li>Grocery</li>
+        <li>Electronics</li>
+        <li>Clothes</li>
+        <li>Hardwares</li>
+    </ul>
+
     
+</body>
 </html>
