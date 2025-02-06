15f260ff966eeb7efe53e5dcbc76b39c65cbd7b3
yoshbatula <yoshbatula2@gmail.com>
2025-02-06 13:56:01 +0800

    progress

--- practice.php
+++ practice.php
@@ -1,13 +1,4 @@
-<?php ;
-    $cookie_name = "user";
-    $cookie_value = "John Doe";
-
-    setcookie($cookie_name, $cookie_value, time() + (66400 * 30),"/");
-
-    if (isset($_COOKIE[$cookie_name])) {
-        echo "Cookie named " . $cookie_name . "is not set!";
-    } else {
-        echo "Cookie " . $cookie_name . " is set! <br>";
-        echo "Value is: " . $cookie_value . "is set <br>";
-    }
-?>
+<!DOCTYPE html>
+<html lang="en">
+    
+</html>
