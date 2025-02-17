7b5e1b4eb5d5c1c1e9d45470d2478f09ab721f60
yoshbatula <yoshbatula2@gmail.com>
2025-02-18 02:52:32 +0800

    case statement

--- case.php
+++ case.php
@@ -1,7 +1,24 @@
 <?php
-    $grade = "";
+    $grade = "B";
 
     switch ($grade) {
-        
+        case "A":
+            echo "You did fucking great";
+            break;
+        case "B":
+            echo "You did a good job";
+            break;
+        case "C":
+            echo "You did good";
+            break;
+        case "D":
+            echo "Good job";
+            break;
+        case "F":
+            echo "You failed";
+            break;
+        default:
+            echo "{$grade} that's not a grade";
+            break;
     }
 ?>
